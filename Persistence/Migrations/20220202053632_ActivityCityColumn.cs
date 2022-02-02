a62accbf6730abe2ea45813e3e9f5fa9b6a037e5
Pedro Cunha <pedrovcunha87@gmail.com>
2022-02-02 17:05:14 +1100

    Initial

---
+++ Persistence/Migrations/20220202053632_ActivityCityColumn.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class ActivityCityColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyPropCityerty",
+                table: "Activities",
+                newName: "Venue");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Activities");
+
+            migrationBuilder.RenameColumn(
+                name: "Venue",
+                table: "Activities",
+                newName: "MyPropCityerty");
+        }
+    }
+}
